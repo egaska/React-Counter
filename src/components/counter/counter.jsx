6310247dd4189e3214cb532b6be8d60d1ac18842
Emily Gaska <emilygaska@gmail.com>
2020-08-13 13:16:52 -0500

    Set up first component to display

---
+++ src/components/counter/counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Counter(){
+
+    return(
+        <div className= "thirdComponent">
+            This is in Counter!
+        </div>
+    )
+}
+export default Counter;
